57667b7206d22438dc4ab71f8c0e972ff3a586f7
Michael Mendy <montana@linux.com>
2023-11-10 09:42:36 -0800

    Push up `mock_aws_keys.sh`

--- mock_aws_keys.sh
+++ mock_aws_keys.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
 generate_access_key_id() {
-    # AWS Access Key IDs are 20 characters long with uppercase alpha-numeric characters
     echo "$(tr -dc 'A-Z0-9' < /dev/urandom | head -c 20)"
 }
 
 generate_secret_access_key() {
-    # AWS Secret Access Keys are 40 characters long with base64 characters
     echo "$(tr -dc 'A-Za-z0-9+/' < /dev/urandom | head -c 40)"
 }
 
